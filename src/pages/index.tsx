68ac752541a32abd5ea038c69d416f55aad6d0aa
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-05 22:21:33 +1200

    Separate out components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,6 @@
-import { Inter } from 'next/font/google'
 import Nav from '@/components/Nav'
-
-const inter = Inter({ subsets: ['latin'] })
-
-type List = {
-  id: number
-  name: string
-  items: Item[]
-}
-
-type Item = {
-  id: number
-  content: string
-}
-
-type CardProps = {
-  list: List
-}
+import Card from '@/components/Card'
+import type { List } from '@/utils/Types'
 
 const data: List[] = [
   {
@@ -58,23 +42,6 @@ const data: List[] = [
     ],
   },
 ]
-
-const Card = ({ list }: CardProps) => {
-  return (
-    <div className="font-inter h-96 w-72 py-4 text-base">
-      <h2 className="text-center font-bold leading-5 tracking-tight">
-        {list.name}
-      </h2>
-      <ul className="bg-gray-300">
-        {list.items.map((item) => (
-          <li key={item.id} className="rounded-lg bg-white px-4 py-2">
-            {item.content}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
 
 const Home = () => {
   return (
