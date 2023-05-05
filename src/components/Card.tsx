9eb64353a3aca4ffebdedec17ef8bc8b1b5bc9ae
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-05 23:02:33 +1200

    Card styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import { List } from '@/utils/Types'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 type CardProps = {
   list: List
@@ -25,28 +22,38 @@ const Card = ({ list }: CardProps) => {
   }
 
   return (
-    <div className="font-inter h-96 w-72 bg-gray-300 p-4 py-4 text-base">
-      <h2 className="text-center font-bold leading-5 tracking-tight">
+    <div className="flex flex-col justify-end">
+      <h2 className="mb-2 text-center font-bold leading-5 tracking-tight">
         {list.name}
       </h2>
-      <ul className="space-y-4">
-        {list.items.map((item) => (
-          <li key={item.id} className="rounded-lg bg-white px-4 py-2">
-            {item.content}
-          </li>
-        ))}
-      </ul>
-      <form className="space-y-4" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={item}
-          onChange={(e) => setItem(e.target.value)}
-          className="rounded-lg bg-white p-2 text-black"
-        />
-        <button type="submit" className="rounded-lg bg-white p-2 text-black">
-          GO
-        </button>
-      </form>
+      <div className="mb-4 h-96 w-72 rounded-lg bg-[#D9D9D9] p-4 py-4 text-base">
+        <div className="flex h-full flex-col justify-between">
+          <ul className="space-y-4">
+            {list.items.map((item) => (
+              <li key={item.id} className="rounded-lg bg-white px-4 py-2">
+                {item.content}
+              </li>
+            ))}
+          </ul>
+          <form
+            className="mt-auto flex items-center gap-2"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              value={item}
+              onChange={(e) => setItem(e.target.value)}
+              className="w-52 rounded-lg bg-white p-2 text-black"
+            />
+            <button
+              type="submit"
+              className="rounded-lg bg-white p-2 text-xs text-black"
+            >
+              GO
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
